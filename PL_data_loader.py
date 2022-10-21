3a602a93e649bbea1066eb1281d6ea316d21c638
Vinod Appajanna <vinapp@users.noreply.github.com>
2022-10-21 11:38:05 +0530

    Update PL_data_loader.py

--- PL_data_loader.py
+++ PL_data_loader.py
@@ -3,15 +3,9 @@
 import gdown
 import argparse
 
-file_destinations = {'FaceMaskDetection':'Face Mask Detection.zip',
-              'COVIDTimeSeries':'COVIDTimeSeries.zip',
-              'GAN-Colorization':'Victorian400-GAN-colorization-data.zip',
-              'NLP-QG':'CoNLL+BEA_corrected_essays.pkl'}
+file_destinations = {'NatarajaAbhaMudraDetection':'Face Mask Detection.zip'}
 
-file_id_dic = {'FaceMaskDetection':'1pJtohTc9NGNRzHj5IsySR39JIRPfkgD3',
-              'COVIDTimeSeries':'1B36Djko-f0LtVxSaBEVYosLTTsu0qtsh',
-              'GAN-Colorization':'1tPNnwPISjPaC1nDZ1QpXcJAQCFmeO3Uv',
-              'NLP-QG':'1dPa5iLEv6WqeHZaYhNLPw5q_i5IGJzU9'}
+file_id_dic = {'NatarajaAbhaMudraDetection':'1Cw-cDkMswFN-dzW1k0vScAKmKmZoYgH6'}
 
 def download_file_from_google_drive(id_, destination):
     url = f'https://drive.google.com/uc?id={id_}'
